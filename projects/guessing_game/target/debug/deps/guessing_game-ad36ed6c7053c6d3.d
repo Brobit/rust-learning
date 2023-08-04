42db964ee8abc0dc530a8c0d48e0881447cb7bef
brobit <brobit@pop-os.localdomain>
2023-08-04 23:29:40 +0200

    add first part of guessing game

---
+++ projects/guessing_game/target/debug/deps/guessing_game-ad36ed6c7053c6d3.d
@@ -0,0 +1,5 @@
+/home/brobit/programmation/rust_learning/projects/guessing_game/target/debug/deps/guessing_game-ad36ed6c7053c6d3: src/main.rs
+
+/home/brobit/programmation/rust_learning/projects/guessing_game/target/debug/deps/guessing_game-ad36ed6c7053c6d3.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/brobit/programmation/rust_learning/projects/guessing_game/target/debug/guessing_game: /home/brobit/programmation/rust_learning/projects/guessing_game/src/main.rs
