237dcf53c7b808f7e7ee3def71ac02781fc94dcd
brobit <alannmaricourt15@gmail.com>
2023-08-10 23:26:00 +0200

    end introduction to function chapter

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,9 +1,30 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5);
+    print_label_measurement(5, 'h');
+
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_label_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
