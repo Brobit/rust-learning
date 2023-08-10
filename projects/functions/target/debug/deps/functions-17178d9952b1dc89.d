61f230a58f8b613ece82824c29ff105951c61281
brobit <alannmaricourt15@gmail.com>
2023-08-10 21:31:14 +0200

    beggin how funciton work

---
+++ projects/functions/target/debug/deps/functions-17178d9952b1dc89.d
@@ -0,0 +1,5 @@
+/home/brobit/programmation/rust_learning/projects/functions/target/debug/deps/functions-17178d9952b1dc89: src/main.rs
+
+/home/brobit/programmation/rust_learning/projects/functions/target/debug/deps/functions-17178d9952b1dc89.d: src/main.rs
+
+src/main.rs:
---
+++ projects/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/brobit/programmation/rust_learning/projects/functions/target/debug/functions: /home/brobit/programmation/rust_learning/projects/functions/src/main.rs
