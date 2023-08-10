c673d325add6ce2e8327c333f8c0226b68db7ec9
brobit <alannmaricourt15@gmail.com>
2023-08-10 16:09:19 +0200

    add variables project

---
+++ projects/variables/target/debug/deps/variables-af3b7adf1b067932.d
@@ -0,0 +1,5 @@
+/home/brobit/programmation/rust_learning/projects/variables/target/debug/deps/variables-af3b7adf1b067932: src/main.rs
+
+/home/brobit/programmation/rust_learning/projects/variables/target/debug/deps/variables-af3b7adf1b067932.d: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/brobit/programmation/rust_learning/projects/variables/target/debug/variables: /home/brobit/programmation/rust_learning/projects/variables/src/main.rs
