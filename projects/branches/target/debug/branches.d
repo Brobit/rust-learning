2db1a00da28b55fe7c224e63f467151be14eee49
brobit <alannmaricourt15@gmail.com>
2023-08-11 22:12:43 +0200

    add branches section

---
+++ projects/branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/brobit/programmation/rust_learning/projects/branches/target/debug/branches: /home/brobit/programmation/rust_learning/projects/branches/src/main.rs
---
+++ projects/branches/target/debug/deps/branches-e89625dcb5fd010e.d
@@ -0,0 +1,5 @@
+/home/brobit/programmation/rust_learning/projects/branches/target/debug/deps/branches-e89625dcb5fd010e: src/main.rs
+
+/home/brobit/programmation/rust_learning/projects/branches/target/debug/deps/branches-e89625dcb5fd010e.d: src/main.rs
+
+src/main.rs:
