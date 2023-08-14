08f8c3c99c204b30211442f266449c04b673e6e9
brobit <alannmaricourt15@gmail.com>
2023-08-14 23:08:10 +0200

    add the fahrenheit to celius project

---
+++ projects/fahr_cels/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // get the input from the user
+    // to select he right convertion to do, make 2 fonction:
+    // if argv[2] == celsius, convert argv[2] to celsius;
+    // if argv[2] == fahr, convert argv[2] to fahr.
+    // two more fonction :
+    // one to convert celsius to fahr,
+    // the other to convert fahr to celius
+}
